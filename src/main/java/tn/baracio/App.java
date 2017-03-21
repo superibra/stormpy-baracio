29a1446e3e92413c298ad732cad61cebc0d1c78d
superibra <amine.bouharb@esprit.tn>
2017-03-21 16:50:14 +0100

    Initial commit

---
+++ src/main/java/tn/baracio/App.java
@@ -0,0 +1,13 @@
+package tn.baracio;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
